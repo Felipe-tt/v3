8bed07b72b075a75dde2c099884aa070a4d27630
Felipe-tt <felipeittner5@gmail.com>
2022-07-06 15:41:57 -0300

    round --fix

--- spmktcashier.py
+++ spmktcashier.py
@@ -1,3 +1,4 @@
+from math import floor
 IValue,ProductList,ClientDiscountPoints,CPF,CPFI,ClientPoints = [],[],[],[],[],[]
 def validate_cpf(cpf):
     if len(cpf) != 11:
@@ -33,12 +34,11 @@ while True:
                     if CPF[i] == ClientCPF: 
                         if type(ClientDiscountPoints[i]) != float: ClientDiscountPoints[i] = ValueSum/5
                         else: ClientDiscountPoints[i] + ValueSum/5
-                        ClientI = i
             break
     if isSigned: 
         Choose = int(input('Deseja utilizar todos os seus pontos? Sim[1] Não [2]\n'))
         if Choose == 1: ValueSum -= (ClientDiscountPoints[ClientI] * 0.25); ClientDiscountPoints[ClientI] = 0
-        print(f'CPF do cliente: {ClientCPF}\nQuantidade de pontos: {round(ClientDiscountPoints[ClientI])}\nTotal a pagar: {ValueSum}')
+        print(f'CPF do cliente: {ClientCPF}\nQuantidade de pontos: {floor(ClientDiscountPoints[ClientI])}\nTotal a pagar: {ValueSum}')
     else: print(f'Lista de compras: {ProductList}\nTotal a pagar: {ValueSum}')
     ValueSum = 0; IValue.clear(); ProductList.clear()
     Choose = int(input("Deseja fazer uma nova compra? Sim[1] Não[2]\n"))
