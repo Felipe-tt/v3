4db07332a61239e0b843ef361bd3a3c7d956f754
Felipe-tt <felipeittner5@gmail.com>
2022-07-07 13:47:07 -0300

    main

--- spmktcashier.py
+++ spmktcashier.py
@@ -39,7 +39,7 @@ while True:
         Choose = int(input('Deseja utilizar todos os seus pontos? Sim[1] Não [2]\n'))
         if Choose == 1: ValueSum -= (ClientDiscountPoints[ClientI] * 0.25); ClientDiscountPoints[ClientI] = 0
         print(f'CPF do cliente: {ClientCPF}\nQuantidade de pontos: {floor(ClientDiscountPoints[ClientI])}\nTotal a pagar: {ValueSum}')
-    else: print(f'Lista de compras: {",".join(ProductList)}\nTotal a pagar: {ValueSum}')
+    else: print(f'Lista de compras: {",".join(ProductList)}\nTotal a pagar: R${ValueSum}')
     ValueSum = 0; IValue.clear(); ProductList.clear()
     Choose = int(input("Deseja fazer uma nova compra? Sim[1] Não[2]\n"))
     if Choose == 2: break
